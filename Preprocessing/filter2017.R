4f45bc8b057411fa7ce8002d9361417d05108499
abhivij <abhishek.kcvijayan@gmail.com>
2020-02-06 17:00:13 +1100

    normalized 2017 data

--- Preprocessing/filter2017.R
+++ Preprocessing/filter2017.R
@@ -1,3 +1,5 @@
+library(edgeR)
+
 data2017 <- read.table("data/input/2017/GSE89843_TEP_Count_Matrix.txt", header=TRUE)
 count_data <- data2017[,-1]
 rownames(count_data) <- data2017[,1]
@@ -8,8 +10,6 @@ rownames(count_data) <- data2017[,1]
 # healthy_status <- data.frame(factor(patient_info$Classification_group))
 # rownames(healthy_status) <- gsub('-', '.', patient_info$Sample_name)
 # colnames(healthy_status) = "status"
-
-library(edgeR)
 
 #the below disease_data contains repeats with 'Please note that the mapped and counted intron-spanning reads from both files corresponding to this sample were merged in silico to yield sufficient mapped reads for further analyses.'
 # library(GEOquery)
@@ -30,19 +30,37 @@ disease_data$disease_status <- factor(disease_data$disease_status)
 
 
 # filtering count_data
-cpm_data <- cpm(count_data)
+# cpm_data <- cpm(count_data)
 
 # colnames(count_data[colSums(count_data) == min(colSums(count_data))])
 
-min_total_count_genes <- sort((colSums(count_data)))[1:4]
-par(mfrow = c(2, 2))
+# min_total_count_genes <- sort((colSums(count_data)))[1:4]
+# par(mfrow = c(2, 2))
+# 
+# for(i in c(1:length(min_total_count_genes))){
+#   gene <- names(min_total_count_genes)[i]
+#   plot(cpm_data[, gene], count_data[, gene], main=gene, pch = i, ylim = c(0, 50), xlim = c(0, 1000))
+# }
+# 
+# threshold <- cpm_data > 1
+# keep <- rowSums(threshold) >= 377
+# summary(keep)
+# filtered_data <- count_data[keep, ]
 
-for(i in c(1:length(min_total_count_genes))){
-  gene <- names(min_total_count_genes)[i]
-  plot(cpm_data[, gene], count_data[, gene], main=gene, pch = i, ylim = c(0, 50), xlim = c(0, 1000))
-}
-
-threshold <- cpm_data > 1
-keep <- rowSums(threshold) >= 377
-summary(keep)
+keep <- filterByExpr(count_data)
 filtered_data <- count_data[keep, ]
+
+y <- DGEList(filtered_data)
+y <- calcNormFactors(y)
+group <- disease_data$disease_status
+
+design <- model.matrix(~ 0 + group)
+colnames(design) <- levels(group)
+
+par(mfrow=c(1,1))
+v <- voom(y,design,plot = TRUE)
+
+normalized_data <- v$E
+colnames(normalized_data) <- disease_data$disease_status
+
+write.csv(normalized_data, "data/output/normalized_NSCLC_data.csv")
