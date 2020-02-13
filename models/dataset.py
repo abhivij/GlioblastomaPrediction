14843ae68a5ca1285d7bdfb55dce223fe2960a74
abhivij <abhishek.kcvijayan@gmail.com>
2020-02-13 13:17:11 +1100

    keeping common tag to label mapping

--- models/dataset.py
+++ models/dataset.py
@@ -7,12 +7,14 @@ import torch
 
 class Dataset(data.Dataset):
 
-    def __init__(self, path):
+    def __init__(self, path, print_details = False):
+
+        self.tag_to_label = {b'Cancer':1, b'NonCancer':0}
 
         with open(path, 'r') as f:
             string = f.readline()
 
-        self.tags = string.strip().split(sep=',')
+        self.tags = string.replace('"', '').strip().split(sep=',')
         self.tags.pop(0)
 
         data = np.genfromtxt(path, delimiter=',')
@@ -29,26 +31,19 @@ class Dataset(data.Dataset):
         np.random.shuffle(s)
 
         self.data = self.data[s]
+
         self.tags = self.tags[s]
 
-        self.labels = {}
-        i = 0
-        for tag in self.tags:
-            if tag not in self.labels:
-                self.labels[tag] = i
-                i += 1
-
-        self.num_classes = len(self.labels)
         self.num_features = len(self.data[0][0])
 
-        print("Data tensor shape : ", self.data.shape)
-        print("Num Features : %2d" % self.num_features)
-        print("Num Classes : %2d" % self.num_classes)
+        if print_details:
+            print("Data tensor shape : ", self.data.shape)
+            print("Num Features : %2d" % self.num_features)
 
 
     def __getitem__(self, index):
         return torch.from_numpy(self.data[index]).float(), \
-               torch.from_numpy(np.array(self.labels[self.tags[index]])).long()
+               torch.from_numpy(np.array(self.tag_to_label[self.tags[index]])).long()
 
 
     def __len__(self):
