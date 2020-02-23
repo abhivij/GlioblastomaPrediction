398a86d45db3fcdc52c131a9feaccdc6ff3b8bd3
abhivij <abhishek.kcvijayan@gmail.com>
2020-02-24 01:04:08 +1100

    writeMetrics bug fix

--- writeMetrics.R
+++ writeMetrics.R
@@ -1,7 +1,7 @@
 acc <- read.csv("../models/model_acc.csv", header=FALSE)
 acc_transpose <- as.data.frame(t(as.matrix(acc)))
 
-auc <- read.csv("../models/model_acc.csv", header=FALSE)
+auc <- read.csv("../models/model_auc.csv", header=FALSE)
 auc_transpose <- as.data.frame(t(as.matrix(auc)))
 
 
